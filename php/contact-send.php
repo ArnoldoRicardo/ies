f2da06107966ac28c0bacfdd14d59c925acd43f4
Arnoldo R. L. Fuentes <anyback@gmail.com>
2013-10-12 14:28:50 -0500

    manda correos

--- php/contact-send.php
+++ php/contact-send.php
@@ -1,49 +1,31 @@
 <?php
+extract($_POST);
+if(isset($enviar))
+{
+	if($name==NULL || $subject==NULL || $message==NULL)
+	{
+		error("Hay campos vacios");
+	}
+	else
+	{
+		$mimail="contacto@iesfrosur.edu.mx";
+		$headers = "MIME-Version: 1.0\n";
+		$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+		$headers .= "X-Priority: 3\n";
+		$headers .= "X-MSMail-Priority: Normal\n";
+		$headers .= "X-Mailer: php\n";
+		$headers .= "From: \"".$name."\" <".$email.">\n";
+		$asunto.="Mensaje desde www.frontera-comalapa.com";
+		if(mail($mimail,$subject,$message,$headers)){
+			echo "mensaje enviado";
+		}else{
+			echo "fallo!";
+		}
 
-if($_POST['submit']){
-	$name = $_POST['name'];
-	$mensaje = $_POST['message'];
-	$remitente = $_POST['email'];
-	$destinatario = "anyback@gmail.com";
-	$asunto = $_POST['subject']; 
-	$cuerpo = '
-	<html> 
-	<head> 
-	<title>mensaje enviado desde iesfrosur.edu.mx</title> 
-	</head> 
-	<body>
-	'.$mensaje.'
-	</body> 
-	</html> 
-	'; 
-
-	//Envío en formato HTML 
-	$headers = "MIME-Version: 1.0\r\n"; 
-	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-
-	//Dirección del remitente 
-	$headers .= "From: ".$name." <".$remitente.">\r\n";
-
-	//Dirección de respuesta (Puede ser una diferente a la de pepito@mydomain.com)
-	$headers .= "Reply-To: ".$remitente."\r\n"; 
-
-	//Ruta del mensaje desde origen a destino 
-	$headers .= "Return-path: holahola@mydomain.com\r\n"; 
-
-	//direcciones que recibián copia 
-	//$headers .= "Cc: maria@mydomain.com\r\n"; 
-
-	//Direcciones que recibirán copia oculta 
-	//$headers .= "Bcc: pepe@pepe.com, juan@juan.com\r\n"; 
-
-	mail($destinatario,$asunto,$cuerpo,$headers);
+	}
 
 }
-else{
-	?><script type="text/javascript">
-
-	console.log("mesaje no eviado");
-	
-	</script><?php
+else {
+	echo "mensaje no enviado";
 }
 ?>
